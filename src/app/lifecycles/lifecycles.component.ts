582e405ef3ad23f38264dcdcbdc92de0dc74ee78
Alexsfv <safronov.sanya37@gmail.com>
2021-09-03 10:07:41 +0500

    template references

--- src/app/lifecycles/lifecycles.component.ts
+++ src/app/lifecycles/lifecycles.component.ts
@@ -9,7 +9,8 @@ import {
   AfterViewInit,
   AfterViewChecked,
   Input,
-
+  ViewChild,
+  ContentChild,
 } from '@angular/core';
 
 @Component({
@@ -24,6 +25,12 @@ import {
         <p>Property: </p>
         <div>{{text}}</div>
       </div>
+      <div>
+        <p #boundParagraph>
+          Reference paragraph
+        </p>
+        <i>{{boundParagraph.textContent}}</i>
+      </div>
     </div>
   `,
   styles: [`
@@ -35,37 +42,55 @@ import {
 export class LifecyclesComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   @Input() text: string = ''
 
+  @ViewChild('boundParagraph') boundParagraph: HTMLElement;
+  @ContentChild('boundContent') boundContent: HTMLElement;
+
   constructor() { }
 
   ngOnInit() {
     console.log("onInit")
+    // console.log('p', this.boundParagraph)
+    // console.log('p', this.boundContent)
   }
 
   ngDoCheck() {
     console.log("doCheck")
+    // console.log('p', this.boundParagraph)
+    // console.log('p', this.boundContent)
   }
 
-  ngOnChanges() {
-    console.log("onChanges")
+  ngOnChanges(changes) {
+    // console.log('p', this.boundParagraph)
+    // console.log('p', this.boundContent)
   }
 
   ngAfterContentInit() {
     console.log("afterContentInit")
+    // console.log('p', this.boundParagraph)
+    // console.log('p', this.boundContent)
   }
 
   ngAfterContentChecked() {
     console.log("afterContentChecked")
+    // console.log('p', this.boundParagraph)
+    // console.log('p', this.boundContent)
   }
 
   ngAfterViewInit() {
     console.log("afterViewInit")
+    // console.log('p', this.boundParagraph)
+    // console.log('p', this.boundContent)
   }
 
   ngAfterViewChecked() {
     console.log("afterViewChecked")
+    // console.log('p', this.boundParagraph)
+    // console.log('p', this.boundContent)
   }
 
   ngOnDestroy() {
     console.log("onDestroy")
+    // console.log('p', this.boundParagraph)
+    // console.log('p', this.boundContent)
   }
 }
